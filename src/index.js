96f9cef8a428f1afa4c04158facf6713cbd98e3c
tourist-17 <yadsak00@gmail.com>
2024-02-22 14:35:54 +0530

    First commit to Basic setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
+const setupAndStartServer = async () => {
+  // create the express object
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, async () => {
+    console.log(`Server Started at ${PORT}`);
+  });
+};
+setupAndStartServer();
